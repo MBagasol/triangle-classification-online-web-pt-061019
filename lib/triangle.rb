b9d86e23435616b895b878db766e9b1350702c98
Mark Anthony Bagasol <markbagasol@gmail.com>
2019-08-05 00:50:08 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,14 @@ class Triangle
   end
 
   def kind
-    if a == b && b == c
+    
+    if a <= 0 || b <= 0 || c <=0 
+      raise TriangleError
+    elsif (a + b) < c || (a + c) <= b || (b + c) < a 
+      raise TriangleError
+    
+    
+    elsif a == b && b == c
       :equilateral
     elsif a == b || b == c || a == c
       :isosceles
